eae1b50db9186e4c26ac143e258a152b75be4ecf
Bladish <Bladish@users.noreply.github.com>
2018-09-12 12:46:39 +0200

    parabolicCurves done

--- parabolicCurves/build-tmp/source/parabolicCurves.java
+++ parabolicCurves/build-tmp/source/parabolicCurves.java
@@ -16,20 +16,23 @@ public class parabolicCurves extends PApplet {
 
 int xCord = 80;
 int yCord = 600;
-int distance = 50;
+int distance = 30;
 public void setup(){
   
-  background(255);
-  stroke(0,0,0,255);
-  strokeWeight(5.5f);
+  background(0,191,255);
+  strokeWeight(2.5f);
 }
 
 public void draw(){
 	for (int i = 0; i < width; i = i + distance) {
-		line(0,i,xCord + i,yCord);	
+		line(0,i,xCord + i,yCord);
+		if(i % (3 * distance) == 0){
+			stroke(0);
+		}else stroke(220,220,220);
 	}
   	
-  }
+ }
+
   public void settings() {  size(600, 600); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "parabolicCurves" };
--- parabolicCurves/parabolicCurves.pde
+++ parabolicCurves/parabolicCurves.pde
@@ -1,16 +1,19 @@
 int xCord = 80;
 int yCord = 600;
-int distance = 50;
+int distance = 30;
 void setup(){
   size(600, 600);
-  background(255);
-  stroke(0,0,0,255);
-  strokeWeight(5.5);
+  background(0,191,255);
+  strokeWeight(2.5);
 }
 
 void draw(){
 	for (int i = 0; i < width; i = i + distance) {
-		line(0,i,xCord + i,yCord);	
+		line(0,i,xCord + i,yCord);
+		if(i % (3 * distance) == 0){
+			stroke(0);
+		}else stroke(220,220,220);
 	}
   	
  }
+
