5f4f464a055363266ae8acf77699f607cc3bb1e4
Bladish <Bladish@users.noreply.github.com>
2018-09-12 15:56:44 +0200

    parabolicCurves new class

--- parabolicCurves/build-tmp/source/parabolicCurves.java
+++ parabolicCurves/build-tmp/source/parabolicCurves.java
@@ -17,6 +17,7 @@ public class parabolicCurves extends PApplet {
 int xCord = 0;
 int yCord = 600;
 int distance = 30;
+int[] position = new int[2];
 public void setup(){
   
   background(0,191,255);
@@ -24,14 +25,36 @@ public void setup(){
 }
 
 public void draw(){
-	for (int i = 0; i < width; i = i + distance) {
+	//drawParabolicCurve();
+  	ParabolicCurves2 parabolicCurves  = new ParabolicCurves2(position,0,600,8);
+
+ }
+
+ public void drawParabolicCurve() {
+ 	for (int i = 0; i < width; i = i + distance) {
 		line(0,i,xCord + i,yCord);
 		if(i % (3 * distance) == 0){
 			stroke(0);
 		}else stroke(220,220,220);
 	}
-  	
+ 	
  }
+
+ public class ParabolicCurves2 {
+ 	private int[] position;
+ 	private int axis1;
+ 	private int axis2;
+ 	private int numberOfLines; 
+ 	
+	public ParabolicCurves2 (int[] position, int axis1, int axis2, int numberOfLines ) {
+		int lineDistance = width/numberOfLines;	
+		for (int i = 0; i < numberOfLines; i++) {
+			line(0, i * lineDistance, axis1 + i * lineDistance, axis2);		
+			}		
+	}
+
+
+}
 
   public void settings() {  size(600, 600); }
   static public void main(String[] passedArgs) {
--- parabolicCurves/parabolicCurves.pde
+++ parabolicCurves/parabolicCurves.pde
@@ -1,6 +1,7 @@
 int xCord = 0;
 int yCord = 600;
 int distance = 30;
+int[] position = new int[4];
 void setup(){
   size(600, 600);
   background(0,191,255);
@@ -8,12 +9,34 @@ void setup(){
 }
 
 void draw(){
-	for (int i = 0; i < width; i = i + distance) {
+	//drawParabolicCurve();
+  	ParabolicCurves2 parabolicCurves  = new ParabolicCurves2(position,0,600,8);
+
+ }
+
+ public void drawParabolicCurve() {
+ 	for (int i = 0; i < width; i = i + distance) {
 		line(0,i,xCord + i,yCord);
 		if(i % (3 * distance) == 0){
 			stroke(0);
 		}else stroke(220,220,220);
 	}
-  	
+ 	
  }
+
+ public class ParabolicCurves2 {
+ 	private int[] position;
+ 	private int axis1;
+ 	private int axis2;
+ 	private int numberOfLines; 
+ 	
+	public ParabolicCurves2 (int[] position, int axis1, int axis2, int numberOfLines ) {
+		int lineDistance = width/numberOfLines;	
+		for (int i = 0; i < numberOfLines; i++) {
+			line(0, i * lineDistance, axis1 + i * lineDistance, axis2);		
+			}		
+	}
+
+
+}
 
